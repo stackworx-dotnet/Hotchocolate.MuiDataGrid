fc53e58964ed886f2ed5e5680bae440ccbb1c93a
Ciaran Liedeman <ciaran@stackworx.io>
2023-04-21 13:46:31 +0200

    chore: fmt

--- src/Hotchocolate.MuiDataGrid/DefaultGuidHandler.cs
+++ src/Hotchocolate.MuiDataGrid/DefaultGuidHandler.cs
@@ -9,63 +9,63 @@ public class DefaultGuidHandler<T> : ExpressionBuilderHandler<T>
         switch (filter.OperatorValue)
         {
             case "equals":
-            {
-                var val = this.GetValueConstantExpression(member, filter);
-                expression = Expression.Equal(memberAccessor, val);
-                break;
-            }
+                {
+                    var val = this.GetValueConstantExpression(member, filter);
+                    expression = Expression.Equal(memberAccessor, val);
+                    break;
+                }
 
             case "isEmpty":
-            {
-                var method = typeof(string).GetMethod("IsNullOrEmpty", new[] { typeof(string) })!;
-                var callExpression = Expression.Call(method, memberAccessor);
-
-                if (member.IsNullable && flavour == ExpressionBuilderFlavour.IN_MEMORY)
                 {
-                    expression = this.WrapWithNullCheck(member.Expression, callExpression);
-                }
-                else
-                {
-                    expression = callExpression;
-                }
+                    var method = typeof(string).GetMethod("IsNullOrEmpty", new[] { typeof(string) })!;
+                    var callExpression = Expression.Call(method, memberAccessor);
 
-                break;
-            }
+                    if (member.IsNullable && flavour == ExpressionBuilderFlavour.IN_MEMORY)
+                    {
+                        expression = this.WrapWithNullCheck(member.Expression, callExpression);
+                    }
+                    else
+                    {
+                        expression = callExpression;
+                    }
+
+                    break;
+                }
 
             case "isNotEmpty":
-            {
-                var method = typeof(string).GetMethod("IsNullOrEmpty", new[] { typeof(string) })!;
-                var callExpression = Expression.Not(Expression.Call(method, memberAccessor));
-
-                if (member.IsNullable && flavour == ExpressionBuilderFlavour.IN_MEMORY)
                 {
-                    expression = this.WrapWithNullCheck(member.Expression, callExpression);
-                }
-                else
-                {
-                    expression = callExpression;
-                }
+                    var method = typeof(string).GetMethod("IsNullOrEmpty", new[] { typeof(string) })!;
+                    var callExpression = Expression.Not(Expression.Call(method, memberAccessor));
 
-                break;
-            }
+                    if (member.IsNullable && flavour == ExpressionBuilderFlavour.IN_MEMORY)
+                    {
+                        expression = this.WrapWithNullCheck(member.Expression, callExpression);
+                    }
+                    else
+                    {
+                        expression = callExpression;
+                    }
+
+                    break;
+                }
 
             case "isAnyOf":
-            {
-                var values = this.GetValueConstantExpressionList(member, filter);
-                var method = typeof(ICollection<string>).GetMethod("Contains")!;
-                var callExpression = Expression.Call(values, method, memberAccessor);
-
-                if (member.IsNullable && flavour == ExpressionBuilderFlavour.IN_MEMORY)
                 {
-                    expression = this.WrapWithNullCheck(member.Expression, callExpression);
-                }
-                else
-                {
-                    expression = callExpression;
-                }
+                    var values = this.GetValueConstantExpressionList(member, filter);
+                    var method = typeof(ICollection<string>).GetMethod("Contains")!;
+                    var callExpression = Expression.Call(values, method, memberAccessor);
 
-                break;
-            }
+                    if (member.IsNullable && flavour == ExpressionBuilderFlavour.IN_MEMORY)
+                    {
+                        expression = this.WrapWithNullCheck(member.Expression, callExpression);
+                    }
+                    else
+                    {
+                        expression = callExpression;
+                    }
+
+                    break;
+                }
 
             default:
                 throw new Exception($"Unknown operator: {filter.OperatorValue}");

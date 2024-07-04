cdfce82fe359755200740be9a922079073ba7ef5
Lloyd <lloyd@stackworx.io>
2024-07-04 14:55:20 +0200

    fix: remove deps

--- tests/Hotchocolate.MuiDataGrid.Test/MuiDataInMemoryTests.cs
+++ tests/Hotchocolate.MuiDataGrid.Test/MuiDataInMemoryTests.cs
@@ -6,15 +6,8 @@ using Stackworx.Hotchocolate.Muidatagrid.Entities;
 using Stackworx.Hotchocolate.Muidatagrid.GraphQL;
 using Xunit.Abstractions;
 
-public class MuiDataInMemoryTests
+public class MuiDataInMemoryTests(ITestOutputHelper outputHelper)
 {
-    private ITestOutputHelper outputHelper;
-
-    public MuiDataInMemoryTests(ITestOutputHelper outputHelper)
-    {
-        this.outputHelper = outputHelper;
-    }
-
     [Fact]
     public void TestInMemory()
     {
@@ -22,23 +15,24 @@ public class MuiDataInMemoryTests
 
         var people = new List<Person>
         {
-            new Person
+            new()
             {
                 Bio = null,
             },
-            new Person
+            new()
             {
                 Bio = "My Bio",
             },
         };
-        var filters = builder.Filter(new MuiDataGridFilterInput
-        {
-            Items = new List<MuiDataGridFilterItemInput>
+        var filters = builder.Filter(
+            new MuiDataGridFilterInput
             {
-                new MuiDataGridFilterItemInput("bio", new MuiValue("Jan"), "contains"),
-            },
-        });
-        this.outputHelper.WriteLine(filters.ToString());
+                Items = new List<MuiDataGridFilterItemInput>
+                {
+                    new("bio", new MuiValue("Jan"), "contains"),
+                },
+            });
+        outputHelper.WriteLine(filters.ToString());
         var results = people.AsQueryable().Where(filters).ToList();
         // var results = people.AsQueryable().Where(p => ((p.Bio != null) && p.Bio.Contains("Jan"))).ToList();
 
@@ -50,13 +44,13 @@ public class MuiDataInMemoryTests
     {
         var nullabilityContext = new NullabilityInfoContext();
 
-        Expression<Func<Person, string?>> e1 = (Person p) => p.Bio;
-        Expression<Func<Person, string>> e2 = (Person p) => p.Firstname;
+        Expression<Func<Person, string?>> e1 = p => p.Bio;
+        Expression<Func<Person, string>> e2 = p => p.Firstname;
 
         MemberExpression t1 = (MemberExpression)e1.Body;
         MemberExpression t2 = (MemberExpression)e2.Body;
         var ctx1 = nullabilityContext.Create((PropertyInfo)t1.Member);
         var ctx2 = nullabilityContext.Create((PropertyInfo)t2.Member);
-        Console.WriteLine(ctx1);
+        outputHelper.WriteLine(ctx1.ToString());
     }
 }

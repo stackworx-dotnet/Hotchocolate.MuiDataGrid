926cb51f2086babf822e59da03dad17b10721c32
Lloyd <lloyd@stackworx.io>
2024-07-04 14:51:00 +0200

    fix: no restore

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-dotnet test ./tests/Hotchocolate.MuiDataGrid.Test --configuration=Release --no-build --no-restore --logger "trx;LogFileName=TestResults.Integration.trx"
+dotnet test ./tests/Hotchocolate.MuiDataGrid.Test --configuration=Release --no-build --logger "trx;LogFileName=TestResults.Integration.trx"

6df067ebe9d3b6b3bd2eb52a58b329b11d3df118
Lloyd <lloyd@stackworx.io>
2024-07-04 13:30:43 +0200

    feat: create package

--- tests/Hotchocolate.MuiDataGrid.Test/ClientQueryRequest.cs
+++ tests/Hotchocolate.MuiDataGrid.Test/ClientQueryRequest.cs
@@ -1,58 +1,36 @@
 namespace Stackworx.Hotchocolate.MuiDataGrid;
 
 using System.Text;
-using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 public class ClientQueryRequest
 {
-    [JsonProperty("id")]
+    [JsonPropertyName("id")]
     public string Id { get; set; } = string.Empty;
 
-    [JsonProperty("operationName")]
+    [JsonPropertyName("operationName")]
     public string OperationName { get; set; } = string.Empty;
 
-    [JsonProperty("query")]
+    [JsonPropertyName("query")]
     public string Query { get; set; } = string.Empty;
 
-    [JsonProperty("variables")]
+    [JsonPropertyName("variables")]
     public Dictionary<string, object> Variables { get; set; } = new();
 
-    [JsonProperty("extensions")]
+    [JsonPropertyName("extensions")]
     public Dictionary<string, object> Extensions { get; set; } = new();
 
     public override string ToString()
     {
         var query = new StringBuilder();
 
-        if (this.Id is not null)
-        {
-            query.Append($"id={this.Id}");
-        }
-
-        if (this.Query is not null)
-        {
-            if (this.Id is not null)
-            {
-                query.Append("&");
-            }
-
-            query.Append($"query={this.Query.Replace("\r", string.Empty).Replace("\n", string.Empty)}");
-        }
-
-        if (this.OperationName is not null)
-        {
-            query.Append($"&operationName={this.OperationName}");
-        }
-
-        if (this.Variables is not null)
-        {
-            query.Append("&variables=" + JsonConvert.SerializeObject(this.Variables));
-        }
-
-        if (this.Extensions is not null)
-        {
-            query.Append("&extensions=" + JsonConvert.SerializeObject(this.Extensions));
-        }
+        query.Append($"id={this.Id}");
+        query.Append('&');
+        query.Append($"query={this.Query.Replace("\r", string.Empty).Replace("\n", string.Empty)}");
+        query.Append($"&operationName={this.OperationName}");
+        query.Append("&variables=" + JsonSerializer.Serialize(this.Variables));
+        query.Append("&extensions=" + JsonSerializer.Serialize(this.Extensions));
 
         return query.ToString();
     }
--- tests/Hotchocolate.MuiDataGrid.Test/TestServerExtensions.cs
+++ tests/Hotchocolate.MuiDataGrid.Test/TestServerExtensions.cs
@@ -2,8 +2,8 @@ namespace Stackworx.Hotchocolate.MuiDataGrid;
 
 using System.Net;
 using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.TestHost;
-using Newtonsoft.Json;
 
 public static class TestServerExtensions
 {
@@ -12,16 +12,21 @@ public static class TestServerExtensions
         ClientQueryRequest request,
         string path = "/graphql")
     {
-        var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+        var content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
         HttpResponseMessage response = await testServer.CreateClient().PostAsync(CreateUrl(path), content);
 
         if (response.StatusCode == HttpStatusCode.NotFound)
         {
-            return new ClientQueryResult { StatusCode = HttpStatusCode.NotFound };
+            return new ClientQueryResult
+            {
+                StatusCode = HttpStatusCode.NotFound,
+            };
         }
 
         var json = await response.Content.ReadAsStringAsync();
-        ClientQueryResult result = JsonConvert.DeserializeObject<ClientQueryResult>(json);
+        var result = JsonSerializer.Deserialize<ClientQueryResult>(json);
+        ArgumentNullException.ThrowIfNull(result);
+
         result.StatusCode = response.StatusCode;
         result.ContentType = response.Content.Headers.ContentType!.ToString();
         return result;

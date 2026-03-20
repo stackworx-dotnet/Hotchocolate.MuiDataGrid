4197dadc825471245784ea3893e0a36e49253953
Ciaran Liedeman <ciaran@stackworx.io>
2026-03-20 14:15:30 +0200

    chore: remove json

--- src/Hotchocolate.MuiDataGrid/MuiDataGridFilterInput.cs
+++ src/Hotchocolate.MuiDataGrid/MuiDataGridFilterInput.cs
@@ -1,8 +1,6 @@
 namespace Stackworx.Hotchocolate.MuiDataGrid;
 
-using System.Text.Json.Serialization;
 using HotChocolate;
-using Stackworx.Hotchocolate.MuiDataGrid.Json;
 using Stackworx.Hotchocolate.MuiDataGrid.Types;
 
 // https://github.com/mui/mui-x/blob/master/packages/grid/x-data-grid/src/models/gridFilterModel.ts
@@ -10,13 +8,10 @@ public record MuiDataGridFilterInput
 {
     public IList<MuiDataGridFilterItemInput> Items { get; set; } = new List<MuiDataGridFilterItemInput>();
 
-    [JsonConverter(typeof(MuiDataGridLinkOperatorConverter))]
     public MuiDataGridLogicOperator? LogicOperator { get; set; }
 
     [property: GraphQLType(typeof(MuiValueType))]
-    [property: JsonConverter(typeof(MuiValueConverter))]
     public MuiValue? QuickFilterValues { get; set; }
 
-    [JsonConverter(typeof(MuiDataGridLinkOperatorConverter))]
     public MuiDataGridLogicOperator? QuickFilterLogicOperator { get; set; }
 }
--- src/Hotchocolate.MuiDataGrid/MuiDataGridFilterItemInput.cs
+++ src/Hotchocolate.MuiDataGrid/MuiDataGridFilterItemInput.cs
@@ -1,15 +1,13 @@
 namespace Stackworx.Hotchocolate.MuiDataGrid;
 
-using System.Text.Json.Serialization;
 using HotChocolate;
 using HotChocolate.Types;
-using Stackworx.Hotchocolate.MuiDataGrid.Json;
 using Stackworx.Hotchocolate.MuiDataGrid.Types;
 
 // https://github.com/mui/mui-x/blob/master/packages/grid/x-data-grid/src/models/gridFilterItem.ts
 public record MuiDataGridFilterItemInput(
     string Field,
-    [property: GraphQLType(typeof(MuiValueType)), JsonConverter(typeof(MuiValueConverter))]
+    [property: GraphQLType(typeof(MuiValueType))]
     MuiValue? Value,
     string Operator,
     [property: GraphQLType(typeof(AnyType))]
